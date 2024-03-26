39a5ab0c8bb7795fe39d76d712e0cf13ebab77a8
rishabhrajyadav <rishabhrajyadav10@gmail.com>
2024-03-26 18:20:40 +0530

    commit

---
+++ script.js
@@ -0,0 +1,141 @@
+import crypto from 'crypto';
+
+const LEFT = 'left';
+const RIGHT = 'right';
+
+const hashes = [
+    '95cd603fe577fa9548ec0c9b50b067566fe07c8af6acba45f6196f3a15d511f6',
+    '709b55bd3da0f5a838125bd0ee20c5bfdd7caba173912d4281cae816b79a201b',
+    '27ca64c092a959c7edc525ed45e845b1de6a7590d173fd2fad9133c8a779a1e3',
+    '1f3cb18e896256d7d6bb8c11a6ec71f005c75de05e39beae5d93bbd1e2c8b7a9',
+    '41b637cfd9eb3e2f60f734f9ca44e5c1559c6f481d49d6ed6891f3e9a086ac78',
+    'a8c0cce8bb067e91cf2766c26be4e5d7cfba3d3323dc19d08a834391a1ce5acf',
+    'd20a624740ce1b7e2c74659bb291f665c021d202be02d13ce27feb067eeec837',
+    '281b9dba10658c86d0c3c267b82b8972b6c7b41285f60ce2054211e69dd89e15',
+    'df743dd1973e1c7d46968720b931af0afa8ec5e8412f9420006b7b4fa660ba8d',
+    '3e812f40cd8e4ca3a92972610409922dedf1c0dbc68394fcb1c8f188a42655e2',
+    '3ebc2bd1d73e4f2f1f2af086ad724c98c8030f74c0c2be6c2d6fd538c711f35c',
+    '9789f4e2339193149452c1a42cded34f7a301a13196cd8200246af7cc1e33c3b',
+    'aefe99f12345aabc4aa2f000181008843c8abf57ccf394710b2c48ed38e1a66a',
+    '64f662d104723a4326096ffd92954e24f2bf5c3ad374f04b10fcc735bc901a4d',
+    '95a73895c9c6ee0fadb8d7da2fac25eb523fc582dc12c40ec793f0c1a70893b4',
+    '315987563da5a1f3967053d445f73107ed6388270b00fb99a9aaa26c56ecba2b',
+    '09caa1de14f86c5c19bf53cadc4206fd872a7bf71cda9814b590eb8c6e706fbb',
+    '9d04d59d713b607c81811230645ce40afae2297f1cdc1216c45080a5c2e86a5a',
+    'ab8a58ff2cf9131f9730d94b9d67f087f5d91aebc3c032b6c5b7b810c47e0132',
+    'c7c3f15b67d59190a6bbe5d98d058270aee86fe1468c73e00a4e7dcc7efcd3a0',
+    '27ef2eaa77544d2dd325ce93299fcddef0fae77ae72f510361fa6e5d831610b2'
+];
+
+const sha256 = (data) => {
+    return crypto.createHash('sha256')
+           .update(data)
+           .digest()
+           .toString('hex');
+}
+
+const getLeafNodeDirectionInTheMerkleTree = (hash,MerkleTree) => {
+    const hashIndex = MerkleTree[0].findIndex(h => h === hash);
+    return hashIndex % 2 === 0 ? LEFT : RIGHT;
+}
+
+function ensureEven(hashes){
+    if(hashes.length % 2 !== 0){
+        hashes.push(hashes[hashes.length - 1]);
+    }
+}
+
+function generateMerkleRoot(hashes){
+    if(!hashes || hashes.length == 0){
+        return '';
+    }
+    
+    let newHashes = ensureEven(hashes);
+    let newArr = [];
+
+    for(let i = 0; i < hashes.length - 1; i+= 2){
+        const newHash = hashes[i] + hashes[i+1];
+        const hash = sha256(newHash);
+        newArr.push(hash); 
+    }
+
+    if(newArr.length === 1){
+        return newArr.join('');
+    }
+
+    return generateMerkleRoot(newArr);
+}
+
+function generateMerkleTree(hashes){
+    if(!hashes || hashes.length == 0){
+        return '';
+    }
+
+    const tree = [hashes];
+    
+    const generate = (hashes,tree) => {
+      if(hashes.length === 1){
+          return hashes;
+      }
+
+      ensureEven(hashes);
+      const newArr = [];
+      for(let i = 0; i < hashes.length - 1; i+= 2){
+        const newHash = hashes[i] + hashes[i+1];
+        const hash = sha256(newHash);
+        newArr.push(hash); 
+    }
+      tree.push(newArr);
+      return generate(newArr,tree);
+    }
+    
+    generate(hashes, tree);
+    return tree;
+}
+
+function generatedMerkleProof(hash, hashes) {
+    if(!hash || !hashes || hashes.length === 0) {
+        return null;
+    }
+    const tree = generateMerkleTree(hashes);
+    const merkleProof = [{
+        hash,
+        direction: getLeafNodeDirectionInMerkleTree(hash, tree)
+    }];
+    let hashIndex = tree[0].findIndex(h => h === hash);
+    for(let level = 0; level < tree.length - 1; level++) {
+        const isLeftChild = hashIndex % 2 === 0;
+        const siblingDirection = isLeftChild ? RIGHT : LEFT;
+        const siblingIndex = isLeftChild ? hashIndex + 1 : hashIndex - 1;
+        const siblingNode = {
+            hash: tree[level][siblingIndex],
+            direction: siblingDirection
+        };
+        merkleProof.push(siblingNode);
+        hashIndex = Math.floor(hashIndex / 2);
+    }
+    return merkleProof;
+}
+
+function getMerkleRootFromMerkleProof(merkleProof){
+    if(!merkleProof || merkleProof.length === 0){
+        return '';
+    }
+
+    const merkleRootfromProof = merkleProof.reduce((hashProof1,hashProof2) => {
+        if(hashProof1.direction === LEFT){
+            const hash = sha256(hashProof1.hash + hashProof2.hash)
+            return hash;
+          }else{
+            const hash = sha256(hashProof2.hash + hashProof1.hash);
+            return hash;
+        }
+    })
+
+    return merkleRootfromProof.hash;
+}
+
+const merkleRootFromMerkleProof = getMerkleRootFromMerkleProof(generatedMerkleProof);
+
+console.log('merkleRootFromMerkleProof: ', merkleRootFromMerkleProof);
+console.log('merkleRootFromMerkleProof === merkleRoot: ', merkleRootFromMerkleProof === merkleRoot);
